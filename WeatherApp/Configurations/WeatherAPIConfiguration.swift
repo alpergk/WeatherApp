952da537483bc8c270425c3b81237e2a78425f8c
alpergok <alpergok92@outlook.com>
2025-03-15 22:10:40 +0300

    added lan,lon params, horizontal collection view to show weather properties on the UI

--- WeatherApp/Configurations/WeatherAPIConfiguration.swift
+++ WeatherApp/Configurations/WeatherAPIConfiguration.swift
@@ -10,6 +10,7 @@ import NetworkLayer
 
 
 
+
 //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ef6b401f3a917b4a419a9908342eeff1
 //https://api.openweathermap.org/data/2.5/weather?q=Yalova&appid=ef6b401f3a917b4a419a9908342eeff1
 //https://openweathermap.org/img/wn/10d@2x.png
